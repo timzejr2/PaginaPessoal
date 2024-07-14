07a40a46d34359b82af2676798740a6fa6198d89
Valentim <timzejr2@gmail.com>
2024-07-13 21:38:51 -0300

    novo balao de fala

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbCarouselConfig, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgIf } from '@angular/common';
 
@@ -7,15 +7,23 @@ import { NgIf } from '@angular/common';
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
-export class CarouselComponent {
-  //images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
-  //images = ['https://postsimages.blob.core.windows.net/images/4d0ba369-5975-480c-918c-a8fc8e52237b-Blog.png', 'https://postsimages.blob.core.windows.net/images/7f186a84-2888-4811-b234-06cf1e33c9e7-Projetos.png']
+export class CarouselComponent implements OnInit {
+  frases: string[] = [
+    'Olá, Seja bem vindo!',
+    'Eu sou Valentim!',
+    'Desenvolvedor Dotnet e Angular.',
+    'Criei esta página para postar algumas coisas que venho estudando.',
+    'E alguns projetos pessoais.'
+  ];
 
-  constructor(config: NgbCarouselConfig) {
-		// customize default values of carousels used by this component tree
-		config.interval = 5000;
-		config.wrap = true;
-		config.keyboard = false;
-		config.pauseOnHover = false;
+  fraseAtual: string = "";
+  indexAtual: number = 0;
+
+  ngOnInit(): void {
+    this.fraseAtual = this.frases[this.indexAtual];
+    setInterval(() => {
+      this.indexAtual = (this.indexAtual + 1) % this.frases.length;
+      this.fraseAtual = this.frases[this.indexAtual];
+    }, 5000);
   }
 }
